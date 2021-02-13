4c4cf3db352515068b9370d74cd71c4bf443e16e
uc-mitchbr <77466767+uc-mitchbr@users.noreply.github.com>
2021-02-13 15:30:34 -0500

    Delete powershell directory

--- powershell/lab3.ps1
+++
@@ -1,7 +0,0 @@
-﻿$IP = (Get-NetIPAddress).ipv4address | Select-String "192*"
-$USER = $HOST.Name
-$HOSTNAME = [System.Net.Dns]::GetHostName()
-$VERSION = $HOST.Version.Major
-$TODAY = (Get-Date -format 'D')
-
-$BODY = "The Machine's IP address is $IP. The User is $USER. The Hostname is $HOSTNAME. Powershell version is $VERSION. Today's Date is $TODAY."
