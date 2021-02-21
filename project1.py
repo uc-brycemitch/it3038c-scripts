749559828b050a2f92f3a2e33d321eca8eeda73d
uc-mitchbr <mitchbr@mail.uc.edu>
2021-02-21 12:23:51 -0500

    'project1'

---
+++ project1.py
@@ -0,0 +1,9 @@
+#imported datetime to get current date and time
+import datetime;
+
+#created variable currentTime and used "now" to get the current time
+currentTime = datetime.datetime.now();
+
+#printing out the date and time to the terminal
+print("The date and time on the VM is: ");
+print (currentTime);
